e46162e75b331bc59284c204186b87d64ccff19b
edpyt <edrsa1321s@gmail.com>
2024-05-07 17:06:46 +0300

    refactor `parse_mitigration.py`

--- src/parse_mitigation.py
+++ src/parse_mitigation.py
@@ -2,8 +2,8 @@ import os
 
 import html_to_json
 from constants import (
-    get_tmfk_source,
     MITIGATIONS_PATH,
+    get_tmfk_source,
 )
 from marko.ext.gfm import gfm
 from stix2 import CourseOfAction
@@ -11,6 +11,7 @@ from stix2 import CourseOfAction
 
 def handle_description_markup(description_row: dict) -> str:
     mdescription = ""
+
     if "code" in description_row:
         codes = [c["_value"] for c in description_row["code"]]
         if "_values" in description_row:
@@ -21,29 +22,43 @@ def handle_description_markup(description_row: dict) -> str:
             mdescription = description_row["_value"] + codes[0]
     else:
         mdescription = description_row["_value"]
+
     return mdescription
 
-def craft_mitigation_url(tmfk_id, mitigation_name, parent_mitigations):
+
+def craft_mitigation_url(
+    tmfk_id: str, mitigation_name: str, parent_mitigations: list
+) -> str:
     mid = "/"
     if len(parent_mitigations) != 0:
         mid = f"/{parent_mitigations[0]}/"
-    return "https://microsoft.github.io/Threat-Matrix-for-Kubernetes/mitigations" + mid + f"{tmfk_id}%20{mitigation_name.replace(' ', '%20')}/"
+    return (
+        "https://microsoft.github.io/Threat-Matrix-for-Kubernetes/mitigations"
+        + mid
+        + f"{tmfk_id}%20{mitigation_name.replace(' ', '%20')}/"
+    )
 
-def parse_mitigation(file_path: str):
+
+def parse_mitigation(file_path: str) -> tuple[CourseOfAction, list]:
     with open(file_path, "r", encoding="utf-8") as f:
         content = f.read()
         html_content = gfm(content)
         json_content = html_to_json.convert(html_content)
+
         mitigation_name = json_content["h1"][0]["_value"]
         tmfk_id = json_content["p"][1]["_values"][0].split(":")[-1].strip()
+
         mitre_attack_mitigations = []
         parent_mitigations = []
+
         if "_values" in json_content["p"][1]:
-            if "MITRE mitigation: -" not in json_content["p"][1]["_values"]:                  
+            if "MITRE mitigation: -" not in json_content["p"][1]["_values"]:
                 t = [a["_value"] for a in json_content["p"][1]["a"]]
-                mitre_attack_mitigations = list(filter(lambda x: x.startswith("M") and not x.startswith("MS"), t))
+                mitre_attack_mitigations = list(
+                    filter(lambda x: x.startswith("M") and not x.startswith("MS"), t)
+                )
                 parent_mitigations = list(filter(lambda x: x.startswith("MS"), t))
-        
+
         parent_mitigation = None
         if len(parent_mitigations) != 0:
             parent_mitigation = parent_mitigations[0]
@@ -53,33 +68,46 @@ def parse_mitigation(file_path: str):
             external_references=[
                 {
                     "source_name": f"{get_tmfk_source()}",
-                    "url": craft_mitigation_url(tmfk_id=tmfk_id, mitigation_name=mitigation_name, parent_mitigations=parent_mitigations),
+                    "url": craft_mitigation_url(
+                        tmfk_id=tmfk_id,
+                        mitigation_name=mitigation_name,
+                        parent_mitigations=parent_mitigations,
+                    ),
                     "external_id": tmfk_id,
                 }
             ],
             name=mitigation_name,
             description="\n\n".join(
-                [handle_description_markup(d) for d in json_content["p"][2:] if "_value" in d and '!!!' not in d["_value"]]
+                [
+                    handle_description_markup(d)
+                    for d in json_content["p"][2:]
+                    if "_value" in d and "!!!" not in d["_value"]
+                ]
             ),
             x_mitre_ids=mitre_attack_mitigations,
-            x_mitre_parent_mitigation=parent_mitigation
+            x_mitre_parent_mitigation=parent_mitigation,
         )
-       
+
         tids = []
         for row in json_content["table"][0]["tbody"][0]["tr"]:
             tids.append(row["td"][0]["a"][0]["_value"])
 
+        print(type(mitigation), type(tids))
+
         return mitigation, tids
 
 
-def handle_folder(folder: str):
+def handle_folder(folder: str) -> tuple[dict, dict]:
     current_path = MITIGATIONS_PATH / folder
     listing = os.listdir(current_path)
+
     mitigations = {}
     mapping = {}
+
     for _, file_name in enumerate(listing):
         file_path = current_path / file_name
         mitigation, tids = parse_mitigation(file_path=file_path)
         mapping[mitigation.id] = tids
         mitigations[mitigation.id] = mitigation
+
     return mitigations, mapping
